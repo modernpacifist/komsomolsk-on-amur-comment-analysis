2e9d18588838520350529acfe8b3b003b4ca36a4
modernpacifist <modernpacifist@gmail.com>
2023-04-21 16:18:52 +0300

    flattened json function

--- likes_cleanup.py
+++ likes_cleanup.py
@@ -19,6 +19,28 @@ def convert_likes(raw_json):
     return updated_js
 
 
+def flatten_json(y):
+    out = {}
+    def flatten(x, name=''):
+        # If the Nested key-value
+        # pair is of dict type
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], name + a + '_')
+        # If the Nested key-value
+        # pair is of list type
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '_')
+                i += 1
+        else:
+            out[name[:-1]] = x
+ 
+    flatten(y)
+    return out
+
+
 if __name__ == '__main__':
     try:
         with open("./datasets/submissionsWithForms.json") as f:
@@ -28,6 +50,11 @@ if __name__ == '__main__':
         exit(1)
 
     updated_js = convert_likes(js)
+
+    res = []
+    for i in updated_js:
+        res.append(flatten_json(i))
+        # updated_js = flatten_list_of_dicts(updated_js)
 
     # for j in js:
     #     attributes = j.get('attributes')
@@ -39,4 +66,5 @@ if __name__ == '__main__':
 
     # json.dumps()
     with open('./datasets/submissionsWithFormsCleanLikes.json', 'w') as f:
-        json.dump(updated_js, f, ensure_ascii=False, indent=4)
+        # json.dump(updated_js, f, ensure_ascii=False, indent=4)
+        json.dump(res, f, ensure_ascii=False, indent=4)
